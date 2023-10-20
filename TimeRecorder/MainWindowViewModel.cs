887ff1ad1a47499d2c49be785d47f5ff6d1dc95e
cwk <chaewk15@gmail.com>
2023-10-20 13:28:59 +0900

    초기 커밋.

---
+++ TimeRecorder/MainWindowViewModel.cs
@@ -0,0 +1,48 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace TimeRecorder
+{
+    public partial class MainWindowViewModel : ObservableObject
+    {
+        private int rank = 0;
+
+        [ObservableProperty]
+        private string? _currentTime;
+        [ObservableProperty]
+        private ObservableCollection<RecordBox> _recordBoxCollection = new();
+
+        public MainWindowViewModel()
+        {
+            UpdateCurrentTime();
+        }
+
+        private void UpdateCurrentTime()
+        {
+            DispatcherTimer timer = new();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            CurrentTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+
+        [RelayCommand]
+        private void CheckCurrentTime()
+        {
+            rank++;
+            RecordBoxCollection.Add(new RecordBox(rank, DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")));
+        }
+    }
+}
---
+++ TimeRecorder/RecordBox.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace TimeRecorder
+{
+    public class RecordBox : Control
+    {
+        public string? Rank { get; set; }
+        public string? CheckedTime { get; set; }
+
+        static RecordBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(RecordBox), new FrameworkPropertyMetadata(typeof(RecordBox)));
+        }
+
+        public RecordBox(int rank, string checkedTime)
+        {
+            Rank = "#" + rank.ToString();
+            CheckedTime = checkedTime;
+        }
+    }
+}
